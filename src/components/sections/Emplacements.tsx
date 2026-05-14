aa6e2b72202d9709051125d4ba0ee8540c8862a8
Loic-Assogba <loicassogba7@gmail.com>
2026-05-14 13:04:26 +0100

    Précision au niveau des emplacements des sites

--- src/components/sections/Emplacements.tsx
+++ src/components/sections/Emplacements.tsx
@@ -1,21 +1,47 @@
-import { MapPin } from "lucide-react";
-import Image from "next/image";
-import { Card, CardContent } from "@/components/ui/card";
+import { MapPin } from 'lucide-react';
+import Image from 'next/image';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default function Emplacements() {
     return (
         <section className="bg-[#65b3d9] md:flex max-w-7xl mx-auto ">
             <aside className="p-8 md:w-1/2">
-                <h2 className="font-bold text-2xl md:text-3xl mb-8">NOS SITES DE FORMATION</h2>
+                <h2 className="font-bold text-2xl md:text-3xl mb-8">
+                    NOS SITES DE FORMATION
+                </h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <EmplacementsList city="GODOMEY PK 14" detail=" Immeuble du Supermarché Ô Bénin" />
-                    <EmplacementsList city="COTONOU 1" detail=" Menontin, rue du supermarché MontSinaï" />
-                    <EmplacementsList city="BOHICON" detail=" École catholique saint François d'Assise" />
-                    <EmplacementsList city="DASSA" detail=" Collège Catholique Saint Michel de Dassa " />
-                    <EmplacementsList city="PARAKOU" detail=" Université ESAE" />
-                    <EmplacementsList city="NATITINGOU" detail=" EPP Tchriminan" /> 
-                    <EmplacementsList city="COTONOU 2" detail=" Haie-Vive" />
-                    <EmplacementsList city="COTONOU 3 Sèmè-Kpodji" detail="Immeuble du centre de formation CIEFP-Benin " />
+                    <EmplacementsList
+                        city="GODOMEY PK 14"
+                        detail=" Immeuble du Supermarché Ô Bénin"
+                    />
+                    <EmplacementsList
+                        city="COTONOU Mènontin"
+                        detail="Rue de l'entreprise FIFI, école les petits trésors"
+                    />
+                    <EmplacementsList
+                        city="COTONOU Haie vive"
+                        detail="Ecole royaumes des anges"
+                    />
+                    <EmplacementsList
+                        city="COTONOU Avotrou"
+                        detail="Rue de l'hôtel safari, Immeuble du centre de formation CIEFP BÉNIN"
+                    />
+                    <EmplacementsList
+                        city="BOHICON"
+                        detail=" École catholique saint François d'Assise"
+                    />
+                    <EmplacementsList
+                        city="DASSA"
+                        detail=" Collège Catholique Saint Michel de Dassa "
+                    />
+                    <EmplacementsList
+                        city="PARAKOU"
+                        detail=" Université ESAE"
+                    />
+                    <EmplacementsList
+                        city="NATITINGOU"
+                        detail=" EPP Tchriminan"
+                    />
                 </div>
             </aside>
             <aside className="md:w-1/2">
@@ -32,16 +58,22 @@ export default function Emplacements() {
     );
 }
 
-export function EmplacementsList({ city, detail }: { city: string; detail: string}) {
+export function EmplacementsList({
+    city,
+    detail,
+}: {
+    city: string;
+    detail: string;
+}) {
     return (
-                 <Card className="bg-white/20 border-white/30 backdrop-blur-sm">
-                     <CardContent className="flex flex-col  gap-4 p-4 ">
-                         <p className="text-sm md:text-base flex gap-2 "> 
-                             <MapPin className="w-5 h-5 text-white shrink-0" />
-                             <span className="font-bold text-[#24324d]">{city} </span> 
-                        </p>
-                         <span className="text-white">{detail}</span>
-                    </CardContent>
-                </Card>
-    )
+        <Card className="bg-white/20 border-white/30 backdrop-blur-sm">
+            <CardContent className="flex flex-col  gap-4 p-4 ">
+                <p className="text-sm md:text-base flex gap-2 ">
+                    <MapPin className="w-5 h-5 text-white shrink-0" />
+                    <span className="font-bold text-[#24324d]">{city} </span>
+                </p>
+                <span className="text-white">{detail}</span>
+            </CardContent>
+        </Card>
+    );
 }
